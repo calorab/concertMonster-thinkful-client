670a8ae0053fb33913647890d1b54e95244073f2
Caleb Gammon <calebwgammon@gmail.com>
2019-10-25 11:20:04 -0400

    added logo and checklist for <toolbar />

---
+++ src/Containers/hoc/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Layout extends Component {
+    render() {
+        return {
+            // Add toolbar with navigationItems and navItem's
+            // add <Logo /> with some logo I create online
+            // add sideDrawer with toggle 3 stack lines
+            // needs state for open/close sideDrawer and possibly chow/hide NavItem's
+        };
+    }
+};
+
+export default layout;
