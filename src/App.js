28f8bfebb17141434337cb8369a66bbbc5a3eb71
Caleb Gammon <calebwgammon@gmail.com>
2019-10-28 15:54:05 -0400

    added Welcome homepage with button and routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import './App.css';
 import Auth from './Containers/Auth/Auth';
 import Layout from './Containers/hoc/Layout/Layout';
+import Welcome from './Components/Welcome/Welcome';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-            <Auth />
+            <Route path='/' component={Welcome} />
+            <Route path='/auth' component={Auth}/>
         </Layout>
       </div>
     );
---
+++ src/Components/Welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Button from '../UI/Button/Button';
+
+const welcome = () => (
+    <div>
+        <h1>Welcome to Concert Monster!</h1>
+        <Button btnType='Success'>GET STARTED</Button>
+    </div>
+);
+
+export default welcome;
