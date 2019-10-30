366a55a4007640ccc6c180494bd86fbb9b818f3e
Caleb Gammon <calebwgammon@gmail.com>
2019-10-30 17:12:17 -0400

    added link from logo to / as well as <Spinner />

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import classes from './Logo.module.css';
+import {Link} from 'react-router-dom';
 
-import conMonLogo from '../../Assets/Images/concert-monster-logo.png';
+import concertMonsterLogo from '../../Assets/Images/concert-monster-logo.png';
+
 
 
 const logo = (props) => (
-    <div className={classes.Logo} style={{height: props.height}}>
-        <img src={conMonLogo} alt='Concert Monster Logo' />
+    <div className={classes.Logo} >
+        <a href='/'>
+            <img src={concertMonsterLogo} alt='Concert Monster Logo' />
+        </a>
     </div>
 );
 
